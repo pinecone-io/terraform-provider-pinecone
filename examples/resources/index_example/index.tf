45fc1eb1b669c0890b02cf397ee94131b6d32031
Richard Weerasinghe <richard.weerasinghe@youtap.com>
2023-09-10 11:33:28 +1200

    fixed index example

--- examples/resources/index_example/index.tf
+++ examples/resources/index_example/index.tf
@@ -1,9 +1,17 @@
-provider "pinecone" {
-  // Any required provider configuration parameters, e.g., API key, endpoint, etc.
-  api_key = "var.api_key"
+terraform {
+  required_providers {
+    pinecone = {
+      source = "skyscrapr/pinecone"
+    }
+  }
 }
 
-resource "pinecone_index" "example_index" {
-  name = "my_example_index"
-  // Any other attributes or configurations specific to the index creation.
+provider "pinecone" {
+	environment = "gcp-starter"
+}
+
+resource "pinecone_index" "test" {
+  name = "frank"
+  dimension = 512
+  metric = "cosine"
 }
