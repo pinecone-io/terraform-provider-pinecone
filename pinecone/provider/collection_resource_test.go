bd07ec28b71379fc859d5154c88cb137632532ed
Shaun S <shaun.stuart@gmail.com>
2023-09-12 16:08:21 +1200

    added test collection for pinecone api call

--- pinecone/provider/collection_resource_test.go
+++ pinecone/provider/collection_resource_test.go
@@ -1,78 +1,40 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package provider
 
 import (
-	"fmt"
-	sdkacctest "github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/tftest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccIndexResource(t *testing.T) {
-	rName := sdkacctest.RandomWithPrefix("tftest")
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+func TestAccPineconeCollectionResource(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		ProviderFactories: tftest.ProviderFactories(map[string]tfsdk.ProviderFactory{
+			"pinecone": func() (tfsdk.Provider, error) {
+				return New("test"), nil
+			},
+		}),
 		Steps: []resource.TestStep{
-			// Create and Read testing
 			{
-				Config: testAccIndexResourceConfig(rName, 1),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("pinecone_index.test", "id", rName),
-					resource.TestCheckResourceAttr("pinecone_index.test", "name", rName),
-					resource.TestCheckResourceAttr("pinecone_index.test", "dimension", "512"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "metric", "cosine"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "pods", "1"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "replicas", "1"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "pod_type", "starter"),
-					resource.TestCheckNoResourceAttr("pinecone_index.test", "source_collection"),
+				Config: testAccPineconeCollectionResourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("pinecone_collection.test", "name", "test-collection"),
 				),
 			},
-			// ImportState testing
 			{
-				ResourceName:      "pinecone_index.test",
+				ResourceName:      "pinecone_collection.test",
 				ImportState:       true,
 				ImportStateVerify: true,
-				// ImportStateVerifyIdentifierAttribute: "name",
-				// This is not normally necessary, but is here because this
-				// example code does not have an actual upstream service.
-				// Once the Read method is able to refresh information from
-				// the upstream service, this can be removed.
-				// ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
 			},
-			// Update and Read testing
-			{
-				// TODO: update replicas test. Cannot do this currently in the free-tier.
-				Config: testAccIndexResourceConfig(rName, 1),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("pinecone_index.test", "id", rName),
-					resource.TestCheckResourceAttr("pinecone_index.test", "name", rName),
-					resource.TestCheckResourceAttr("pinecone_index.test", "dimension", "512"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "metric", "cosine"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "pods", "1"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "replicas", "1"),
-					resource.TestCheckResourceAttr("pinecone_index.test", "pod_type", "starter"),
-					resource.TestCheckNoResourceAttr("pinecone_index.test", "source_collection"),
-				),
-			},
-			// Delete testing automatically occurs in TestCase
 		},
 	})
 }
 
-func testAccIndexResourceConfig(name string, replicas int) string {
-	return fmt.Sprintf(`
-provider "pinecone" {
-	environment = "gcp-starter"
+func testAccPineconeCollectionResourceConfig() string {
+	return `
+resource "pinecone_collection" "test" {
+  name = "test-collection"
 }
-
-resource "pinecone_index" "test" {
-  name = %q
-  dimension = 512
-  replicas = %d
-}
-`, name, replicas)
+`
 }
