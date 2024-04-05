634eb4cb14ff3f87d9ec0cccb97e1dba63a541f8
gdj0nes <gareth@pinecone.io>
2024-04-05 12:35:03 -0400

    add pod example to resource

--- docs/resources/index.md
+++ docs/resources/index.md
@@ -25,12 +25,26 @@ provider "pinecone" {
   api_key = var.pinecone_api_key
 }
 
-resource "pinecone_index" "test" {
-  name = "tftestindex"
+# Serverless Indexes
+resource "pinecone_index" "serverless_index" {
+  name      = "tftestindex"
+  dimension = 1536
+  metric    = "cosine"
   spec = {
     serverless = {
       cloud  = "aws"
       region = "us-west-2"
+    }
+  }
+}
+# Pod Indexes
+resource "pinecone_index" "pod_index" {
+  name      = "tftestindex"
+  dimension = 1536
+  metric    = "cosine"
+  spec = {
+    pod = {
+      environment = "us-west1-gcp"
     }
   }
 }
--- examples/resources/pinecone_index/resource.tf
+++ examples/resources/pinecone_index/resource.tf
@@ -10,12 +10,26 @@ provider "pinecone" {
   api_key = var.pinecone_api_key
 }
 
-resource "pinecone_index" "test" {
-  name = "tftestindex"
+# Serverless Indexes
+resource "pinecone_index" "serverless_index" {
+  name      = "tf_serverless_index"
+  dimension = 1536
+  metric    = "cosine"
   spec = {
     serverless = {
       cloud  = "aws"
       region = "us-west-2"
+    }
+  }
+}
+# Pod Indexes
+resource "pinecone_index" "pod_index" {
+  name      = "tf_pod_index"
+  dimension = 1536
+  metric    = "cosine"
+  spec = {
+    pod = {
+      environment = "us-west1-gcp"
     }
   }
 }
