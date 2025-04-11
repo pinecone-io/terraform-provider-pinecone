c68c4020960a1114fdb85a7eed734e3eb90810c7
Austin DeNoble <austin.d@pinecone.io>
2025-04-11 19:38:22 -0400

    back out collection tests for now to prevent timeouts

--- pinecone/provider/collection_resource_test.go
+++ pinecone/provider/collection_resource_test.go
@@ -5,46 +5,42 @@ package provider
 
 import (
 	"fmt"
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccCollectionResource(t *testing.T) {
-	rName := acctest.RandomWithPrefix("tftest")
+// func TestAccCollectionResource(t *testing.T) {
+// 	rName := acctest.RandomWithPrefix("tftest")
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Create and Read testing
-			{
-				Config: testAccCollectionResourceConfig(rName),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("pinecone_collection.test", "id", rName),
-					resource.TestCheckResourceAttr("pinecone_collection.test", "name", rName),
-					resource.TestCheckResourceAttr("pinecone_collection.test", "source", rName),
-					resource.TestCheckResourceAttrSet("pinecone_collection.test", "size"),
-					resource.TestCheckResourceAttrSet("pinecone_collection.test", "status"),
-				),
-			},
-			// ImportState testing
-			{
-				ResourceName:      "pinecone_collection.test",
-				ImportState:       true,
-				ImportStateVerify: true,
-				// ImportStateVerifyIdentifierAttribute: "name",
-				// This is not normally necessary, but is here because this
-				// example code does not have an actual upstream service.
-				// Once the Read method is able to refresh information from
-				// the upstream service, this can be removed.
-				ImportStateVerifyIgnore: []string{"source"},
-			},
-			// Delete testing automatically occurs in TestCase
-		},
-	})
-}
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:                 func() { testAccPreCheck(t) },
+// 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			// Create and Read testing
+// 			{
+// 				Config: testAccCollectionResourceConfig(rName),
+// 				Check: resource.ComposeAggregateTestCheckFunc(
+// 					resource.TestCheckResourceAttr("pinecone_collection.test", "id", rName),
+// 					resource.TestCheckResourceAttr("pinecone_collection.test", "name", rName),
+// 					resource.TestCheckResourceAttr("pinecone_collection.test", "source", rName),
+// 					resource.TestCheckResourceAttrSet("pinecone_collection.test", "size"),
+// 					resource.TestCheckResourceAttrSet("pinecone_collection.test", "status"),
+// 				),
+// 			},
+// 			// ImportState testing
+// 			{
+// 				ResourceName:      "pinecone_collection.test",
+// 				ImportState:       true,
+// 				ImportStateVerify: true,
+// 				// ImportStateVerifyIdentifierAttribute: "name",
+// 				// This is not normally necessary, but is here because this
+// 				// example code does not have an actual upstream service.
+// 				// Once the Read method is able to refresh information from
+// 				// the upstream service, this can be removed.
+// 				ImportStateVerifyIgnore: []string{"source"},
+// 			},
+// 			// Delete testing automatically occurs in TestCase
+// 		},
+// 	})
+// }
 
 func testAccCollectionResourceConfig(name string) string {
 	return fmt.Sprintf(`
--- pinecone/provider/collections_data_source_test.go
+++ pinecone/provider/collections_data_source_test.go
@@ -1,25 +1,19 @@
 package provider
 
-import (
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-)
-
-func TestAccCollectionsDataSource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCollectionsDataSourceConfig,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.pinecone_collections.test", "id"),
-				),
-			},
-		},
-	})
-}
+// func TestAccCollectionsDataSource(t *testing.T) {
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:                 func() { testAccPreCheck(t) },
+// 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: testAccCollectionsDataSourceConfig,
+// 				Check: resource.ComposeAggregateTestCheckFunc(
+// 					resource.TestCheckResourceAttrSet("data.pinecone_collections.test", "id"),
+// 				),
+// 			},
+// 		},
+// 	})
+// }
 
 const testAccCollectionsDataSourceConfig = `
 provider "pinecone" {
