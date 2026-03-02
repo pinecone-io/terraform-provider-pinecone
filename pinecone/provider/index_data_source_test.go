76f001a0a6fd84f111eda58a8a068bbb04d1bd13
Austin DeNoble <austin.d@pinecone.io>
2026-03-02 12:08:52 -0500

    add new IndexDataSource tests to validate different varieties of serverless index through a data source

--- pinecone/provider/index_data_source_test.go
+++ pinecone/provider/index_data_source_test.go
@@ -21,7 +21,7 @@ func TestAccIndexDataSource_serverless(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Read testing
 			{
-				Config: testAccIndexDataSourceConfig_serverless(rName),
+				Config: testAccIndexDataSourceConfig_serverlessIntegrated(rName),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.pinecone_index.test", "id", rName),
 					resource.TestCheckResourceAttr("data.pinecone_index.test", "name", rName),
@@ -42,33 +42,191 @@ func TestAccIndexDataSource_serverless(t *testing.T) {
 	})
 }
 
+func TestAccIndexDataSource_serverless_plain(t *testing.T) {
+	t.Parallel()
+	rName := acctest.RandomWithPrefix("tftest")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIndexDataSourceConfig_serverless(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "id", rName),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "name", rName),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "dimension", "1024"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "metric", "cosine"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.cloud", "aws"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.region", "us-west-2"),
+					// Non-integrated index: embed and schema children must be absent.
+					resource.TestCheckNoResourceAttr("data.pinecone_index.test", "embed.model"),
+					resource.TestCheckNoResourceAttr("data.pinecone_index.test", "spec.serverless.schema.fields.%"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIndexDataSource_serverless_withSchema(t *testing.T) {
+	t.Parallel()
+	rName := acctest.RandomWithPrefix("tftest")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIndexDataSourceConfig_serverlessWithSchema(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "id", rName),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "name", rName),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "dimension", "1024"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.cloud", "aws"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.region", "us-west-2"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.schema.fields.%", "2"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.schema.fields.genre.filterable", "true"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.schema.fields.year.filterable", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIndexDataSource_serverless_withReadCapacity(t *testing.T) {
+	t.Parallel()
+	rName := acctest.RandomWithPrefix("tftest")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIndexDataSourceConfig_serverlessWithReadCapacity(rName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "id", rName),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "name", rName),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "dimension", "1024"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.cloud", "aws"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.region", "us-west-2"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.read_capacity.dedicated.node_type", "b1"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.read_capacity.dedicated.replicas", "1"),
+					resource.TestCheckResourceAttr("data.pinecone_index.test", "spec.serverless.read_capacity.dedicated.shards", "1"),
+				),
+			},
+		},
+	})
+}
+
+func testAccIndexDataSourceConfig_serverlessIntegrated(name string) string {
+	return fmt.Sprintf(`
+provider "pinecone" {
+}
+
+resource "pinecone_index" "test" {
+  name = %q
+  spec = {
+    serverless = {
+      cloud  = "aws"
+      region = "us-west-2"
+    }
+  }
+  tags = {
+    test  = "test1"
+    test2 = "test2"
+  }
+  embed = {
+    model = "multilingual-e5-large"
+    field_map = {
+      text = "chunk_text"
+    }
+  }
+}
+
+data "pinecone_index" "test" {
+  name = pinecone_index.test.name
+}
+`, name)
+}
+
 func testAccIndexDataSourceConfig_serverless(name string) string {
 	return fmt.Sprintf(`
-	provider "pinecone" {
-	}
-	
-	resource "pinecone_index" "test" {
-		name = %q
-		spec = {
-		    serverless = {
-		        cloud = "aws"
-			    region = "us-west-2"
-		    }
-		}
-		tags = {
-			test = "test1"
-			test2 = "test2"
-		}
-		embed = {
-			model = "multilingual-e5-large"
-			field_map = {
-				text = "chunk_text"
-			}
-		}
-	}
-	
-	data "pinecone_index" "test" {
-		name = pinecone_index.test.name
-	}
-	`, name)
+provider "pinecone" {
+}
+
+resource "pinecone_index" "test" {
+  name      = %q
+  dimension = 1024
+  spec = {
+    serverless = {
+      cloud  = "aws"
+      region = "us-west-2"
+    }
+  }
+  deletion_protection = "disabled"
+}
+
+data "pinecone_index" "test" {
+  name = pinecone_index.test.name
+}
+`, name)
+}
+
+func testAccIndexDataSourceConfig_serverlessWithSchema(name string) string {
+	return fmt.Sprintf(`
+provider "pinecone" {
+}
+
+resource "pinecone_index" "test" {
+  name      = %q
+  dimension = 1024
+  spec = {
+    serverless = {
+      cloud  = "aws"
+      region = "us-west-2"
+      schema = {
+        fields = {
+          "genre" = { filterable = true }
+          "year"  = { filterable = true }
+        }
+      }
+    }
+  }
+  deletion_protection = "disabled"
+}
+
+data "pinecone_index" "test" {
+  name = pinecone_index.test.name
+}
+`, name)
+}
+
+func testAccIndexDataSourceConfig_serverlessWithReadCapacity(name string) string {
+	return fmt.Sprintf(`
+provider "pinecone" {
+}
+
+resource "pinecone_index" "test" {
+  name      = %q
+  dimension = 1024
+  spec = {
+    serverless = {
+      cloud  = "aws"
+      region = "us-west-2"
+      read_capacity = {
+        dedicated = {
+          node_type = "b1"
+          replicas  = 1
+          shards    = 1
+        }
+      }
+    }
+  }
+  deletion_protection = "disabled"
+}
+
+data "pinecone_index" "test" {
+  name = pinecone_index.test.name
+}
+`, name)
 }
